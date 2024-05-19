f0e03fc54e195fe8bba82addcd43ff97dce10dba
jhutchings99 <jeremyhutchings99@gmail.com>
2024-05-19 13:07:14 -0600

    feat: add legend to all sorting algorithm pages

--- src/public/app/array/cocktail-sort/page.tsx
+++ src/public/app/array/cocktail-sort/page.tsx
@@ -3,6 +3,7 @@ import MainNavigation from "@/components/main-navigation";
 import SortingOptions from "@/components/sorting-options";
 import { generateRandomArray } from "@/utils/generateArray";
 import { Separator } from "@/components/ui/separator";
+import SortingKey from "@/components/sorting-key";
 
 export default function CocktailSort() {
   const arr = generateRandomArray(10);
@@ -11,11 +12,16 @@ export default function CocktailSort() {
   return (
     <div>
       <MainNavigation />
-      <div className="flex">
+      <div className="flex ">
         <SortingOptions />
-        <div className="flex justify-center items-center flex-col">
-          <h1 className="text-xl font-bold py-4 text-center">Cocktail Sort</h1>
-          <Separator />
+        <div className="flex justify-center items-center flex-col ">
+          <div className="w-full">
+            <h1 className="text-xl font-bold py-4 text-center">
+              Cocktail Sort
+            </h1>
+            <Separator />
+            <SortingKey />
+          </div>
 
           <ArrayVisualizer arr={arr} algorithmStr="cocktailSort" />
         </div>
